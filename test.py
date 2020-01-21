a74c36f3e9fad0847d23332591c3acf3921a780e
JessTran7 <35790773+JessTran7@users.noreply.github.com>
2020-01-21 15:41:33 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-hi
+#Opening locations file
+import array
+
+flag = 0 
+connections=[]
+f = open("connections.txt", "r")
+initial_location = input("Enter the initial location: ")
+for line in f:
+    if line.split(None, 1)[0] == initial_location:
+        for x in range(int(line.split(None,1)[1],10)):
+            connections[x] = line.split(None,1)[x+2]
+        flag = 1
+if flag == 0:
+    print("invalid initial location. Default is Set to A1!")
+    initial_location = "A1"
+
+print(initial_location)
+print(connections)   
