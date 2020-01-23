a0e0c100adada88de872c7266aef783d801fc135
JessTran7 <35790773+JessTran7@users.noreply.github.com>
2020-01-23 12:02:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,100 @@
-#Opening locations file
-import array
+import math
 
-flag = 0 
-connections=[]
-f = open("connections.txt", "r")
-initial_location = input("Enter the initial location: ")
-for line in f:
-    if line.split(None, 1)[0] == initial_location:
-        for x in range(int(line.split(None,1)[1],10)):
-            connections[x] = line.split(None,1)[x+2]
-        flag = 1
-if flag == 0:
-    print("invalid initial location. Default is Set to A1!")
-    initial_location = "A1"
+class Node:
+    def __init__(self, loc=None, x=None,y=None):
+        self.x = x
+        self.y = y
+        self.loc = loc
+        self.next = []
+visited = {}
+BreakTheWorld = False
+def main():
+    f = open('locations.txt', 'r')
+    file = f.readlines()
+    locations = []
+    Nodes = []
+    ID = 0
 
-print(initial_location)
-print(connections)   
+    for lines in file:
+        line = lines.rstrip('\n')
+        line = line.split(' ')
+        loc = line[0]
+        if loc == 'END':
+            break
+        x = line[1]
+        y = line[2]
+        Nodes.append(Node(loc,x,y))
+        locations.append(loc)
+        visited[loc] = False
+        ID += 1
+    f.close()
+    
+    f = open('connections.txt', 'r')
+    file = f.readlines()
+    for lines in file:
+        line = lines.rstrip('\n')
+        line = line.split(' ')
+        loc = line[0]
+        #print(loc)
+        if loc == 'END':
+            break
+        count = int(line[1])
+        #print(count)
+        order = []
+        for k in range(count+1):
+            for x in range(count+1):
+                #print(x)
+                if (x == 0):
+                    continue
+
+                elif (x == 1):
+                    continue
+
+                elif (line[x] > line[x+1]):
+                    temp = line[x]
+                    line[x] = line[x+1]
+                    line[x+1] = temp
+
+        for i in range(count):
+            x = locations.index(line[i + 2])
+            Nodes[locations.index(loc)].next.append(Nodes[x])
+        print(line)
+    f.close()
+    start = 'A1'
+    end = 'G1'
+    path = ['B2']*len(locations)
+    length = [0]*len(locations)
+    findPath(Nodes[locations.index(start)], path, end, length, 0)
+    #print(path)
+    #print(sum(length))
+
+
+def findPath(node,path, end, length, count):
+    
+    if node.loc == end:
+        global BreakTheWorld
+        BreakTheWorld = True
+        return
+    if visited[node.loc]:
+        return
+    else:
+        visited[node.loc] = True
+    print(node.loc)
+    for tmp in node.next:
+        dist = distance(node.x,node.y, tmp.x,tmp.y)
+        #print(node.loc, tmp.loc)
+        #length[count] = dist
+        #path[count] = tmp.loc
+        count += 1
+        findPath(tmp, path, end, length, count)
+        if BreakTheWorld:
+            break
+
+
+def distance(x1,y1,x2,y2):
+    x = math.pow(int(x1)-int(x2), 2)
+    y = math.pow(int(y1)-int(y2), 2)
+
+    return math.sqrt(x+y)
+
+main()
